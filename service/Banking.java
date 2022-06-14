46248f02f5aa9704ac2ebeb1495cd189d4f6c336
AzimuddinAhmad <105585567+AzimuddinAhmad@users.noreply.github.com>
2022-06-15 00:03:04 +0530

    Add files via upload
    sorry it is my first time to upload java file.

---
+++ service/Banking.java
@@ -0,0 +1,57 @@
+//		@@ -0,0  +1,89 @@
+
+package com.greatlearning.service;
+
+import java.util.Scanner;
+
+public class Banking {
+
+		Scanner sc = new Scanner (System.in);
+		OTPGenerator otpG = new OTPGenerator();
+		int balance = 1000;
+public void deposit ()  {
+		int amount ;
+System.out.println("Enter the amount to be deposited");
+	amount = sc.nextInt();
+if (amount > 0)     {
+	balance = balance + amount;
+System.out.println("Total balance amount"  + balance);
+}
+else
+{
+System.out.println("Enter the correct amount : ");
+}
+}
+public void withdrawl ()   {
+	
+		int amount;
+	System.out.println("Enter the amount to be withdrawl");
+		amount = sc.nextInt();
+if (amount > 0 && balance - amount >= 0) {  // Balance = 1000 amount = 1500 1000- (-500) = -500
+	balance = balance - amount;
+		System.out.println("Balance amount"    +balance);
+		System.out.println("Amount withdrawl successfully");
+	}    else    {
+		System.out.println("Insufficient balance");
+}
+}
+public void transter ()   {
+	int amount, otp, otpgenerated, bankAcctNo;
+		otpgenerated = otpG.otpGenerated ();
+		System.out.println("Enter the OTP:  ");
+		otp = sc.nextInt ();
+if (otp == otpgenerated)   {
+	System.out.println("Enter the amount to be transferred and Account number");
+	amount = sc.nextInt ();
+	bankAcctNo = sc.nextInt();
+if (balance - amount > 0)  {
+	System.out.println("Amount"  +amount  + "Transferred successfully");
+	balance = balance - amount;
+	System.out.println("Balance"   +balance);
+}    else    {
+	System.out.println("Transferred failed:  ");	}
+}	else	{
+	System.out.println("Invalid otp");  
+}
+}
+}
